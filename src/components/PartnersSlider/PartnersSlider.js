4e151235e98e013658c79c9cb5587a0f319b703f
MelisaAtaseven <atasevenmelisa2@gmail.com>
2024-11-20 18:24:07 +0300

    partners slider

--- src/components/PartnersSlider/PartnersSlider.js
+++ src/components/PartnersSlider/PartnersSlider.js
@@ -2,7 +2,6 @@ import React from "react";
 import "./PartnersSlider.css";
 
 const PartnersSlider = () => {
-  // Define your array of image URLs
   const imageUrls = [
     "/assets/images/Partners/Group 175.png",
     "/assets/images/Partners/image 13.png",
@@ -15,31 +14,25 @@ const PartnersSlider = () => {
     "/assets/images/Partners/image 21.png",
     "/assets/images/Partners/image 22.png",
     "/assets/images/Partners/image 23.png",
-
     "/assets/images/Partners/image 25.png",
     "/assets/images/Partners/image 26.png",
     "/assets/images/Partners/image 27.png",
     "/assets/images/Partners/image 28.png",
     "/assets/images/Partners/image 29.png",
     "/assets/images/Partners/image 30.png",
-
     "/assets/images/Partners/image 32.png",
     "/assets/images/Partners/image 33.png",
-    // Add more URLs as needed
   ];
 
   return (
     <div className="PartnersSlider">
       <div className="PartnersSlider-track">
-        {/* Duplicate the image set to create a looping effect */}
+        {/* Resim setini döngü için çoğalt */}
         {[...Array(2)].map((_, trackIndex) => (
           <React.Fragment key={trackIndex}>
             {imageUrls.map((url, imageIndex) => (
-              <div
-                key={`${trackIndex}-${imageIndex}`}
-                className="PartnersSlider"
-              >
-                <img src={url} alt="" />
+              <div key={`${trackIndex}-${imageIndex}`}>
+                <img src={url} alt={`Partner ${imageIndex + 1}`} />
               </div>
             ))}
           </React.Fragment>
